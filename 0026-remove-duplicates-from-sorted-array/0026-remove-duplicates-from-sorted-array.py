8ad08a5620a463be5ce5e9ba45db0e1439e1dc25
sanchita-takale <64085200+sanchita-takale@users.noreply.github.com>
2025-11-13 23:50:43 +0530

    Time: 0 ms (100%), Space: 18.9 MB (48.4%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,18 +7,18 @@ class Solution:
         #         nums[i] = num
         #         i+=1
         # return i
-        # k = sorted(list((set(nums))))
-        # for num in range (len(k)):
-        #     nums[num]=k[num]
+        k = sorted(set(nums))
+        for num in range (len(k)):
+            nums[num]=k[num]
         # del nums[num+1:]
-        # print(nums)
-        # return (len(nums))
-        unique_nums = sorted(set(nums))
+        print(nums)
+        return (len(k))
+        # unique_nums = sorted(set(nums))
 
-        # Copy back into nums (in-place modification)
-        for i in range(len(unique_nums)):
-            nums[i] = unique_nums[i]
+        # # Copy back into nums (in-place modification)
+        # for i in range(len(unique_nums)):
+        #     nums[i] = unique_nums[i]
 
-        # Return the number of unique elements
-        return len(unique_nums)
+        # # Return the number of unique elements
+        # return len(unique_nums)
         
